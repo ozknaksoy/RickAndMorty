ed2e17b6922da74a6c3d9a886076ad344d727b74
Ozkan Bilal Aksoy <ozkanbilalaksoy@gmail.com>
2023-11-13 18:21:53 +0300

    pagination not completed

--- js/app.js
+++ js/app.js
@@ -3,40 +3,33 @@ import { renderCharacterItems } from "./ui.js";
 
 const listItems = document.querySelector('.js-list-items');
 const BASE_URL = "https://rickandmortyapi.com/api/character";
-const BASE_URL2 = "https://rickandmortyapi.com/api/character/?page=2";
+
 
 let page = document.querySelectorAll(".page");
 
+
 page.forEach(pageClick => {
-    pageClick.addEventListener('click', (event) => {
-        // let change = event.target.tagName;
-     
+    // console.log(pageClick.textContent);
+    pageClick.addEventListener('click', () => {
+        get(`${BASE_URL}/?page=${pageClick.textContent}`)
+            .then((data) => {
+
+                renderCharacterItems(data, listItems);
+            })
     })
 })
 
-const rickAndMorty = (function () {
-    const getData = function () {
-        get(BASE_URL)
-            .then((datas) => {
-                renderCharacterItems(datas, listItems);
-            })
-    }
 
-    const getData2 = function () {
-        get(BASE_URL2)
-            .then((datas) => {
-                renderCharacterItems(datas, listItems);
-            })
-    }
 
-    return {
-        init: function () {
-            getData();
-            getData2();
-        }
-    };
-})();
+const getData = function () {
+    get(BASE_URL)
+        .then((datas) => {
+            renderCharacterItems(datas, listItems);
+        })
+}
+getData();
 
-rickAndMorty.init();
+
+
 
 
