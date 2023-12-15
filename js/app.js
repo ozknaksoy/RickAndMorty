22be310c0e4b17e4ce84d52e87dc52ff61080742
Ozkan Bilal Aksoy <ozkanbilalaksoy@gmail.com>
2023-12-15 15:27:08 +0300

    character traits taken

--- js/app.js
+++ js/app.js
@@ -1,45 +1,51 @@
 import { get } from './request.js';
 import { renderCharacterItems, openFeature } from './ui.js';
 
-const listItems = document.querySelector('.js-list-items');
-const container = document.querySelector('.container');
-const feature = document.querySelectorAll('.feature');
-let page = document.querySelectorAll('.page');
+document.addEventListener('DOMContentLoaded', async function () {
+    const listItems = document.querySelector('.js-list-items');
+    const characterNameSpans = document.getElementsByClassName('character-name');
+    let page = document.querySelectorAll('.page');
+    const characterListGeneral = document.querySelector('.character-list-general');
+    const characterBody = document.querySelector('.character-body');
 
-const URL = {
-    base_url:'https://rickandmortyapi.com/api/character'
-   
-}
+    const URL = {
+        base_url: 'https://rickandmortyapi.com/api/character'
+    }
 
-const getData = function () {
-    get(URL.base_url)
-        .then((datas) => {
-            renderCharacterItems(datas, listItems);
-        })
-}
-getData();
+    const fetchCharactersAndRender = async function () {
+        const characterData = await get(URL.base_url);
+        renderCharacterItems(characterData, listItems);
+    }
+    await fetchCharactersAndRender();
 
-page.forEach(pageClick => {
-    // console.log(pageClick.textContent);
-    pageClick.addEventListener('click', () => {
-        get(`${URL.base_url}/?page=${pageClick.textContent}`)
-            .then((data) => {
-                listItems.innerHTML = '';
-                renderCharacterItems(data, listItems);
-
-            })
-    })
-})  
-feature.forEach(addFeature => {
-    // console.log(addFeature.textContent)
-    addFeature.addEventListener('click', () => {
-        const featureText = addFeature.textContent;
-        get(`${URL.base_url}/${featureText}`)
-        .then((datas) => {
-            openFeature(datas, container)
-        })
-        .catch((error) => {
-            console.error('basarisiz', error)
+    page.forEach(pageClick => {
+        // console.log(pageClick.textContent);
+        pageClick.addEventListener('click', async (e) => {
+            e.preventDefault()
+           await get(`${URL.base_url}/?page=${pageClick.textContent}`)
+                .then((data) => {
+                    listItems.innerHTML = '';
+                    renderCharacterItems(data, listItems);
+                })
         })
     })
+
+
+    const characterNameSpansArray = [...characterNameSpans];
+    characterNameSpansArray.forEach((characterNameSpan) => {
+        // console.log({ id: characterNameSpan.getAttribute('id') })
+        characterNameSpan.addEventListener('click', () => {
+            get(`${URL.base_url}/${characterNameSpan.getAttribute('id')}`)
+                .then((characterDetailData) => {
+                    console.log({ characterDetailData })
+                    characterListGeneral.style.display = 'none';
+                    openFeature(characterDetailData, characterBody);
+                    characterBody.style.display = 'block';
+                })
+                .catch((error) => {
+                    console.error('basarisiz', error)
+                })
+        })
+    })
+
 })
--- js/ui.js
+++ js/ui.js
@@ -1,55 +1,45 @@
 
 export const renderCharacterItems = function ({ results: characters }, listItems) {
-  
-  characters.forEach((data) => {
-    if (listItems !== null) {
-      // item null değilse işlemleri yap
-      // console.log(listItems);
-    listItems.innerHTML += getCharacters(data)
-    }
-  })
-}
-const getCharacters = function (data) {
-
-  const { image, name, species, location } = data
-  const template = `
+  characters.forEach(({ image, name, id }) => {
+    listItems.innerHTML += `
       <div class="card list-item">
         <div class="card-header">
-          <img
-          src="${image}"
-         alt="Character">
-        </div>
-        <div class="card-body">
-          <h2>Character: <a href="/character.html" class="feature">${name}</a></h2>
-          <span>Species: ${species}</span>
-          <span>Location: ${location.name}</span>
-       </div>
-      </div>
- `;
-  return template;
-}
-
-export const openFeature = function (datas, container) {
-  container.innerHTML = getFeature(datas);
-}
-const getFeature = function (datas, event) {
-  event.preventDefault()
-
-  const { image, name, gender, episode, species, status, location } = datas;
-  const template = `
-      <div class="character-container">
-        <div class="character-images">
           <img src="${image}" alt="Character">
         </div>
-        <div class="character-feature">
-          <h2>Character:${name}</a></h2>
-          <span>Gender: ${gender}</span>
-          <span>Episode: ${episode}</span>
-          <span>Species: ${species}</span>
-          <span>Status: ${status}</span>
-          <span>Location: ${location.name}</span>
+        <div class="card-body">
+          <h2>Character: <a id="${id}" class="character-name">${name}</a></h2>
+
+     
         </div>
-     </div>
-      `;
+      </div>
+    `;
+  });
+};
+
+export const openFeature = function (datas, characterBody) {
+  characterBody.innerHTML = getFeature(datas);
+}
+
+const getFeature = function (datas) {
+  
+  
+  const { image, name, gender, origin, species, status, location } = datas;
+  const template = `
+  <div class="character-grid">
+    <div class="character-container">
+    <div class="character-images">
+    <img src="${image}" alt="Character">
+    </div>
+    <div class="character-feature">
+    <h2>Character:<a>${name}</a></h2>
+    <span>Gender: ${gender}</span>
+    <span>Planet: ${origin.name}</span>
+    <span>Species: ${species}</span>
+    <span>Status: ${status}</span>
+    <span>Location: ${location.name}</span>
+    </div>
+    </div>
+  </div>
+  `;
   return template;
 }
