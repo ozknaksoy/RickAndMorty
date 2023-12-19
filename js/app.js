196eb342860c2c825a0fddfec7e3ba4b05b26a40
Ozkan Bilal Aksoy <ozkanbilalaksoy@gmail.com>
2023-12-19 14:35:05 +0300

    finalize

--- js/app.js
+++ js/app.js
@@ -1,51 +1,68 @@
-import { get } from './request.js';
-import { renderCharacterItems, openFeature } from './ui.js';
+import { get } from "./request.js";
+import { renderCharacterItems, openFeature } from "./ui.js";
 
-document.addEventListener('DOMContentLoaded', async function () {
-    const listItems = document.querySelector('.js-list-items');
-    const characterNameSpans = document.getElementsByClassName('character-name');
-    let page = document.querySelectorAll('.page');
-    const characterListGeneral = document.querySelector('.character-list-general');
-    const characterBody = document.querySelector('.character-body');
+document.addEventListener("DOMContentLoaded", async function () {
+  const listItems = document.querySelector(".js-list-items");
+  let page = document.querySelectorAll(".page");
+  const characterListGeneral = document.querySelector(
+    ".character-list-general"
+  );
+  const characterBody = document.querySelector(".character-body");
+  const body = document.querySelector("body");
 
-    const URL = {
-        base_url: 'https://rickandmortyapi.com/api/character'
-    }
+  const URL = {
+    base_url: "https://rickandmortyapi.com/api/character",
+  };
 
-    const fetchCharactersAndRender = async function () {
-        const characterData = await get(URL.base_url);
-        renderCharacterItems(characterData, listItems);
-    }
-    await fetchCharactersAndRender();
+  const getBackButton = () => {
+    const backButton = document.getElementsByClassName("back-button");
 
-    page.forEach(pageClick => {
-        // console.log(pageClick.textContent);
-        pageClick.addEventListener('click', async (e) => {
-            e.preventDefault()
-           await get(`${URL.base_url}/?page=${pageClick.textContent}`)
-                .then((data) => {
-                    listItems.innerHTML = '';
-                    renderCharacterItems(data, listItems);
-                })
-        })
-    })
+    return backButton;
+  };
 
-
-    const characterNameSpansArray = [...characterNameSpans];
+  const updateCharacterNameSpans = () => {
+    const characterNameSpans =
+      document.getElementsByClassName("character-name");
+    let characterNameSpansArray = [...characterNameSpans];
     characterNameSpansArray.forEach((characterNameSpan) => {
-        // console.log({ id: characterNameSpan.getAttribute('id') })
-        characterNameSpan.addEventListener('click', () => {
-            get(`${URL.base_url}/${characterNameSpan.getAttribute('id')}`)
-                .then((characterDetailData) => {
-                    console.log({ characterDetailData })
-                    characterListGeneral.style.display = 'none';
-                    openFeature(characterDetailData, characterBody);
-                    characterBody.style.display = 'block';
-                })
-                .catch((error) => {
-                    console.error('basarisiz', error)
-                })
-        })
-    })
+      characterNameSpan.addEventListener("click", () => {
+        get(`${URL.base_url}/${characterNameSpan.getAttribute("id")}`)
+          .then((characterDetailData) => {
+            openFeature(characterDetailData, characterBody);
+            characterBody.style.display = "block";
+            body.style.overflow = "hidden";
+            const backButton = getBackButton();
+            backButton[0].addEventListener("click", () => {
+              body.style.overflow = "scroll";
+              characterBody.style.display = "none";
+            });
+          })
+          .catch((error) => {
+            console.error("invalid", error);
+          });
+      });
+    });
+  };
 
-})
+  const fetchCharactersAndRender = async function () {
+    const characterData = await get(URL.base_url);
+
+    renderCharacterItems(characterData, listItems);
+
+    updateCharacterNameSpans();
+  };
+  await fetchCharactersAndRender();
+
+  page.forEach((pageClick) => {
+    pageClick.addEventListener("click", async (e) => {
+      e.preventDefault();
+      await get(`${URL.base_url}/?page=${pageClick.textContent}`).then(
+        (data) => {
+          listItems.innerHTML = "";
+          renderCharacterItems(data, listItems);
+          updateCharacterNameSpans();
+        }
+      );
+    });
+  });
+});
--- js/ui.js
+++ js/ui.js
@@ -1,5 +1,7 @@
-
-export const renderCharacterItems = function ({ results: characters }, listItems) {
+export const renderCharacterItems = function (
+  { results: characters },
+  listItems
+) {
   characters.forEach(({ image, name, id }) => {
     listItems.innerHTML += `
       <div class="card list-item">
@@ -8,8 +10,6 @@ export const renderCharacterItems = function ({ results: characters }, listItems
         </div>
         <div class="card-body">
           <h2>Character: <a id="${id}" class="character-name">${name}</a></h2>
-
-     
         </div>
       </div>
     `;
@@ -18,28 +18,27 @@ export const renderCharacterItems = function ({ results: characters }, listItems
 
 export const openFeature = function (datas, characterBody) {
   characterBody.innerHTML = getFeature(datas);
-}
+};
 
 const getFeature = function (datas) {
-  
-  
   const { image, name, gender, origin, species, status, location } = datas;
   const template = `
-  <div class="character-grid">
-    <div class="character-container">
-    <div class="character-images">
-    <img src="${image}" alt="Character">
+    <div class="character-grid">
+      <div class="character-container">
+        <div class="character-images">
+        <img src="${image}" alt="Character">
+        <a class ="back-button"><i class="fa-solid fa-arrow-left-long"></i></a>
+        </div>
+        <div class="character-feature">
+          <h2>Character:<a>${name}</a></h2>
+          <span>Gender: ${gender}</span>
+          <span>Planet: ${origin.name}</span>
+          <span>Species: ${species}</span>
+          <span>Status: ${status}</span>
+          <span>Location: ${location.name}</span>
+        </div>
+      </div>
     </div>
-    <div class="character-feature">
-    <h2>Character:<a>${name}</a></h2>
-    <span>Gender: ${gender}</span>
-    <span>Planet: ${origin.name}</span>
-    <span>Species: ${species}</span>
-    <span>Status: ${status}</span>
-    <span>Location: ${location.name}</span>
-    </div>
-    </div>
-  </div>
   `;
   return template;
-}
+};
